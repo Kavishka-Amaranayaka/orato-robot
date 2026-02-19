5684a0cf8026b08c0593501e0706a1b2f032bcb5
Kavishka-Amaranayaka <w2120410@westminster.ac.uk>
2026-02-19 22:41:28 +0530

    feat: modified the server.js file.

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,5 +43,5 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log(`Server running on port ${PORT}`);
 });
